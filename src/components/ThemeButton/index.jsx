e57fa76426a7df00903580b936f3daf075f500c0
illiakalinin <illinoiz4305@gmail.com>
2023-01-04 22:12:10 +0200

    Add ThemeButton

---
+++ src/components/ThemeButton/index.jsx
@@ -0,0 +1,17 @@
+const ThemeButton = (props) => {
+  console.log(props);
+  const { isLight, changeTheme } = props;
+
+  const inlineStyle = {
+    color: isLight ? "black" : "white",
+    backgroundColor: isLight ? "white" : "black",
+  };
+
+  return (
+    <button onClick={changeTheme} style={inlineStyle}>
+      {isLight ? "Dark" : "Light"}
+    </button>
+  );
+};
+
+export default ThemeButton;
--- src/components/UsersList/index.jsx
+++ src/components/UsersList/index.jsx
@@ -34,7 +34,15 @@ class UsersList extends Component {
   };
 
   mapUser = (u) => {
-    return <UsersListItem key={u.id} user={u} selectUser={this.selectUser} />;
+    const { isLight } = this.props;
+    return (
+      <UsersListItem
+        key={u.id}
+        user={u}
+        selectUser={this.selectUser}
+        isLight={isLight}
+      />
+    );
   };
 
   render() {
