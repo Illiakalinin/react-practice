70add8194e8c75571e75eb12a8cc3de257cb6e04
illiakalinin <illinoiz4305@gmail.com>
2023-01-07 19:34:42 +0200

    Add LoadUsers

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import { Component } from "react";
-import ThemeButton from "./components/ThemeButton";
-import UsersList from "./components/UsersList";
+import UsersLoader from "./components/UsersLoader";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLight: true,
-    };
-  }
-
-  setTheme = () => {
-    const { isLight } = this.state;
-    this.setState({ isLight: !isLight });
-  };
-
-  render() {
-    const { isLight } = this.state;
-    return (
-      <>
-        <ThemeButton isLight={isLight} changeTheme={this.setTheme} />
-        <UsersList isLight={isLight} />
-      </>
-    );
-  }
+function App() {
+  return <UsersLoader />;
 }
 
 export default App;
