50695b1fef94a9e9741ea66056c8cb5a0ecf4ee2
illiakalinin <illinoiz4305@gmail.com>
2023-01-14 19:34:02 +0200

    Add hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import ImageWrapper from "./components/ImageWrapper";
+import React from "react";
+import CounterH from "./components/CounterH";
 
 function App() {
-  return (
-    <ImageWrapper
-      width="400px"
-      height="300px"
-      title="image"
-      tabIndex={1}
-      className="container"
-    >
-      <img
-        src="https://images.pexels.com/photos/11046405/pexels-photo-11046405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="home"
-      />
-    </ImageWrapper>
-  );
+  return <CounterH />;
 }
 
 export default App;
